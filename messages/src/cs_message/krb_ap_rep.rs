a440fbf146b0d57ed23e95c7f1988ed2bfdad8aa
ltp1405 <lamtungpro1405@gmail.com>
2024-08-04 14:25:49 +0700

    add missing fields

--- messages/src/cs_message/krb_ap_rep.rs
+++ messages/src/cs_message/krb_ap_rep.rs
@@ -1,6 +1,8 @@
-use der::asn1::ContextSpecific;
-use der::{Decode, DecodeValue, Encode, EncodeValue, FixedTag, Length, Reader, Sequence, TagNumber, Writer};
 use crate::basic::EncryptedData;
+use der::asn1::ContextSpecific;
+use der::{
+    Decode, DecodeValue, Encode, EncodeValue, FixedTag, Length, Reader, Sequence, TagNumber, Writer,
+};
 
 /// KRB_AP_REP message - 5.5.1
 #[derive(Debug, PartialEq)]
@@ -9,7 +11,7 @@ pub struct KrbApRep {
 }
 
 impl<'a> DecodeValue<'a> for KrbApRep {
-    fn decode_value<R: Reader<'a>>(reader: &mut R, header: der::Header) -> der::Result<Self> {
+    fn decode_value<R: Reader<'a>>(reader: &mut R, _header: der::Header) -> der::Result<Self> {
         let inner = KrbApRepInner::decode(reader)?;
         Ok(Self { inner })
     }
@@ -36,31 +38,41 @@ impl EncodeValue for KrbApRep {
 struct KrbApRepInner {
     pvno: ContextSpecific<u8>,
     msg_type: ContextSpecific<u8>,
-    // TODO: Wait for EncryptedData to be Sequence
-    // enc_part: ContextSpecific<EncryptedData>,
+    enc_part: ContextSpecific<EncryptedData>,
 }
 
 impl KrbApRep {
     pub fn new(enc_part: EncryptedData) -> Self {
         KrbApRep {
             inner: KrbApRepInner {
-                pvno: ContextSpecific { value: 5, tag_number: TagNumber::new(0), tag_mode: der::TagMode::Explicit },
-                msg_type: ContextSpecific { value: 15, tag_number: TagNumber::new(1), tag_mode: der::TagMode::Explicit },
-                // enc_part: ContextSpecific { value: enc_part, tag_number: TagNumber::new(2), tag_mode: der::TagMode::Explicit },
-            }
+                pvno: ContextSpecific {
+                    value: 5,
+                    tag_number: TagNumber::new(0),
+                    tag_mode: der::TagMode::Explicit,
+                },
+                msg_type: ContextSpecific {
+                    value: 15,
+                    tag_number: TagNumber::new(1),
+                    tag_mode: der::TagMode::Explicit,
+                },
+                enc_part: ContextSpecific {
+                    value: enc_part,
+                    tag_number: TagNumber::new(2),
+                    tag_mode: der::TagMode::Explicit,
+                },
+            },
         }
     }
 
-    pub fn pvno(&self) -> u8 {
+    pub const fn pvno(&self) -> u8 {
         self.inner.pvno.value
     }
 
-    pub fn msg_type(&self) -> u8 {
+    pub const fn msg_type(&self) -> u8 {
         self.inner.msg_type.value
     }
 
     pub fn enc_part(&self) -> &EncryptedData {
-        todo!("Wait for EncryptedData to be Sequence")
-        // &self.inner.enc_part.value
+        &self.inner.enc_part.value
     }
 }
