535c5e2fe4cc552451fa49e8651f06bd4531d042
Hoai Nam <hoainamnguyennhu7@gmail.com>
2024-08-06 00:36:10 +0700

    temp test for kdc req body

--- messages/src/spec_as_tgs_exchange/kdc_req_body.rs
+++ messages/src/spec_as_tgs_exchange/kdc_req_body.rs
@@ -126,3 +126,77 @@ impl KdcReqBody {
         self.additional_tickets.as_ref()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+    use der::{Encode, SliceWriter};
+    use super::*;
+    use crate::basic::{KerberosString, PrincipalName, predefined_values::NameType, Realm, OctetString, KerberosFlags, flags};
+    use crate::tickets::Ticket;
+
+    fn sample_data() -> KdcReqBody {
+        KdcReqBody::new(
+            KerberosFlags::builder()
+                .set(flags::FORWARDABLE)
+                .build().unwrap(),
+            Some(PrincipalName::new(
+                NameType::Enterprise,
+                vec![KerberosString::try_from("host".to_string()).unwrap()],
+            ).unwrap()),
+            Realm::new("EXAMPLE.COM").unwrap(),
+            Some(PrincipalName::new(
+                NameType::Principal,
+                vec![KerberosString::try_from("krbtgt".to_string()).unwrap()],
+            ).unwrap()),
+            Some(KerberosTime::from_unix_duration(Duration::from_secs(1)).unwrap()),
+            KerberosTime::from_unix_duration(Duration::from_secs(2)).unwrap(),
+            Some(KerberosTime::from_unix_duration(Duration::from_secs(3)).unwrap()),
+            UInt32::new(b"\x03").unwrap(),
+            SequenceOf::from(vec![Int32::new(b"\x01").unwrap()]),
+            Some(HostAddresses::new()),
+            Some(EncryptedData::new(
+                Int32::new(b"\x01").unwrap(),
+                UInt32::new(b"\x0A").unwrap(),
+                OctetString::new(b"key").unwrap(),
+            )),
+            None
+            // Some(SequenceOf::from(vec![Ticket::new(
+            //     Realm::new("EXAMPLE.COM").unwrap(),
+            //     PrincipalName::new(
+            //         NameType::Principal,
+            //         vec![KerberosString::try_from("krbtgt".to_string()).unwrap()],
+            //     ).unwrap(),
+            //     EncryptedData::new(
+            //         Int32::new(b"\x01").unwrap(),
+            //         UInt32::new(b"\x0A").unwrap(),
+            //         OctetString::new(b"key").unwrap(),
+            //     ),
+            // )])),
+        )
+    }
+
+    #[test]
+    fn kdc_req_body_ser() {
+        let body = sample_data();
+        let mut binding = [0u8; 256];
+        let mut writer: SliceWriter = SliceWriter::new(&mut binding);
+
+        let kdc_options: [u8; 7] = [0x03, 0x05, 0x00, 0x02, 0x00, 0x00, 0x00];
+        writer.encode(body.kdc_options()).unwrap();
+        let compare = writer.finish().unwrap();
+        assert_eq!(compare.len(), kdc_options.len());
+        assert_eq!(compare, kdc_options);
+
+        let mut writer: SliceWriter = SliceWriter::new(&mut binding);
+        writer.encode(body.realm()).unwrap();
+        let realm: [u8; 13] = [0x16, 0x0B, 0x45, 0x58, 0x41, 0x4D, 0x50, 0x4C, 0x45, 0x2E, 0x43, 0x4F, 0x4D];
+        let compare = writer.finish().unwrap();
+        assert_eq!(compare.len(), realm.len());
+        assert_eq!(compare, realm);
+        let mut buf: Vec<u8> = vec![];
+        body.encode_to_vec(&mut buf).unwrap();
+        println!("{:x?}", buf.len());
+        println!("{:x?}", buf);
+    }
+}
