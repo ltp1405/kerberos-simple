d6570eaeee59938aa40299ad344bd87e89c3506a
ntduydu2004 <nguyentrinhduy2003@gmail.com>
2024-08-06 16:41:48 +0700

    refactor: reduce files of kerberos

--- kerberos/src/client/mod.rs
+++ kerberos/src/client/mod.rs
@@ -1,2 +1,21 @@
-pub mod client;
-pub use client::Client;
+use std::net::SocketAddr;
+
+use tokio::net::TcpStream;
+use tokio::io::{AsyncWriteExt, AsyncReadExt};
+use crate::servers::Server;
+
+pub struct Client {
+    addr: SocketAddr,
+}
+
+impl Client {
+    pub fn new(addr: SocketAddr) -> Self {
+        Self { addr }
+    }
+    pub async fn send(&self, bytes: Vec<u8>, destination: Server) -> tokio::io::Result<()> {
+        todo!();
+    }
+    pub async fn receive(&self) -> tokio::io::Result<Vec<u8>> {
+        todo!();
+    }
+}
