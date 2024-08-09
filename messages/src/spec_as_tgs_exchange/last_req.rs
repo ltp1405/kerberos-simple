8f835d2852e7059d6c7eb2c04447e674e06b65f2
Hoai Nam <hoainamnguyennhu7@gmail.com>
2024-08-09 21:26:03 +0700

    add tests for last_req

--- messages/src/spec_as_tgs_exchange/last_req.rs
+++ messages/src/spec_as_tgs_exchange/last_req.rs
@@ -29,3 +29,49 @@ impl LastReqEntry {
 }
 
 pub type LastReq = SequenceOf<LastReqEntry>;
+
+#[cfg(test)]
+mod tests {
+    use crate::basic::{Int32, KerberosTime};
+    use crate::spec_as_tgs_exchange::last_req::{LastReq, LastReqEntry};
+    use der::{Decode, Encode, SliceReader};
+    use std::time::Duration;
+
+    fn sample_data() -> LastReq {
+        vec![
+            LastReqEntry::new(
+                Int32::new(b"1").unwrap(),
+                KerberosTime::from_unix_duration(Duration::from_secs(2)).unwrap(),
+            ),
+            LastReqEntry::new(
+                Int32::new(b"3").unwrap(),
+                KerberosTime::from_unix_duration(Duration::from_secs(4)).unwrap(),
+            ),
+        ]
+    }
+
+    #[test]
+    fn test_primitives() {
+        let data = sample_data();
+        assert_eq!(data.len(), 2);
+        assert_eq!(*data[0].lr_type(), Int32::new(b"1").unwrap());
+        assert_eq!(
+            *data[0].lr_value(),
+            KerberosTime::from_unix_duration(Duration::from_secs(2)).unwrap()
+        );
+        assert_eq!(*data[1].lr_type(), Int32::new(b"3").unwrap());
+        assert_eq!(
+            *data[1].lr_value(),
+            KerberosTime::from_unix_duration(Duration::from_secs(4)).unwrap()
+        );
+    }
+
+    #[test]
+    fn verify_encode_decode() {
+        let data = sample_data();
+        let mut buf = Vec::new();
+        data.encode_to_vec(&mut buf).unwrap();
+        let decoded = LastReq::decode(&mut SliceReader::new(buf.as_mut_slice()).unwrap()).unwrap();
+        assert_eq!(data, decoded);
+    }
+}
