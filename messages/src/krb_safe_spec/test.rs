fdb9f6185006f6c8acedecddfd893aaf94d51b90
ltp1405 <lamtungpro1405@gmail.com>
2024-08-12 23:49:41 +0700

    More tests

--- messages/src/krb_safe_spec/test.rs
+++ messages/src/krb_safe_spec/test.rs
@@ -1,6 +1,8 @@
-use crate::basic::{Checksum, HostAddress, Int32, OctetString};
+use crate::basic::{Checksum, HostAddress, Int32, KerberosTime, OctetString};
 use crate::krb_safe_spec::KrbSafe;
 use der::{Decode, Encode};
+use std::ops::Add;
+use std::time::{Duration, UNIX_EPOCH};
 
 fn minimal_krb_safe() -> KrbSafe {
     KrbSafe::builder()
@@ -29,50 +31,78 @@ fn encode_then_decode() {
 
     assert_eq!(msg, decoded_msg);
 }
-//
-// struct SequenceIter<'a> {
-//     slice: &'a [u8],
-//     current_idx: usize,
-// }
-//
-// impl<'a> SequenceIter<'a> {
-//     fn new(slice: &'a [u8]) -> Self {
-//         assert_eq!(slice[1] as usize, slice.len() - 1, "Incorrect len for sequence");
-//         SequenceIter {
-//             slice,
-//             current_idx: 2,
-//         }
-//     }
-// }
-//
-// impl<'a> std::iter::Iterator for SequenceIter<'a> {
-//     type Item = &'a [u8];
-//
-//     fn next(&mut self) -> Option<Self::Item> {
-//         let item_start = self.current_idx;
-//         let item_end = self.current_idx + self.slice[self.current_idx+1].as
-//         let rs = self.slice[self.current_idx..self.slice]
-//     }
-// }
 
 #[test]
 fn correct_encode_header() {
     let msg = minimal_krb_safe();
 
     let encoded_msg = msg.to_der().unwrap();
-    println!("{:?}", encoded_msg);
-    assert_eq!(encoded_msg[0], 0b0110_0000 + 20); // APPLICATION  20
-    assert_eq!(encoded_msg[2], 0x30); // SEQUENCE
+    #[rustfmt::skip]
+    let expected_encoding = [
+        0b0110_0000+20, 56, 48, 54, // APPLICATION 20
+            160, 3, 2, 1, 5, // pnvo [0] INTEGER
+            161, 3, 2, 1, 20, // msg-type [1] INTEGER
+            162, 25, 48, 23, // safe-body [2] KRB-SAFE-BODY
+                160, 5, 4, 3, 0, 1, 2, // user-data [0] OCTET STRING
+                164, 14, 48, 12, //s-address [1] HostAddress
+                    160, 3, 2, 1, 2,
+                    161, 5, 4, 3, 0, 1, 2,
+            163, 15, 48, 13, // check-sum [3] Checksum
+                160, 4, 2, 2, 1, 2,
+                161, 5, 4, 3, 0, 1, 2,
+    ];
+    assert_eq!(encoded_msg, expected_encoding);
+}
 
-    // pvno
-    assert_eq!(encoded_msg[4], 0b1010_0000); // [0] Integer
+#[test]
+fn encode_optional_fields() {
+    let msg = KrbSafe::builder()
+        .set_user_data(OctetString::new(&[0x0, 0x1, 0x2]).unwrap())
+        .set_timestamp(
+            KerberosTime::from_system_time(UNIX_EPOCH.add(Duration::from_secs(1000000))).unwrap(),
+        )
+        .set_cksum(Checksum::new(
+            Int32::new(&[0x1, 0x2]).unwrap(),
+            OctetString::new(&[0x0, 0x1, 0x2]).unwrap(),
+        ))
+        .set_r_address(
+            HostAddress::try_from(
+                HostAddress::CODES[1],
+                OctetString::new(&[0x1, 0x2, 0x3]).unwrap(),
+            )
+            .unwrap(),
+        )
+        .set_s_address(
+            HostAddress::try_from(
+                HostAddress::CODES[0],
+                OctetString::new(&[0x0, 0x1, 0x2]).unwrap(),
+            )
+            .unwrap(),
+        )
+        .build_unsafe();
 
-    // msg-type
-    let offset = encoded_msg[5] as usize;
-    let next_entry_idx = 5 + offset + 1;
-    assert_eq!(encoded_msg[5 + offset + 1], 0b1010_0001);
+    let encoded = msg.to_der().unwrap();
+    println!("{:?}", encoded);
 
-    let offset = encoded_msg[next_entry_idx + 1] as usize;
-    let next_entry_idx = next_entry_idx + 1 + offset + 1;
-    assert_eq!(encoded_msg[next_entry_idx], 0b1010_0010);
+    #[rustfmt::skip]
+    let expected_encoding = vec![
+        116, 91, 48, 89, // APPLICATION 20
+            160, 3, 2, 1, 5, // pnvo [0] INTEGER
+            161, 3, 2, 1, 20,// msg-type [1] INTEGER
+            162, 60, 48, 58, // safe-body [2] KRB-SAFE-BODY
+                160, 5, 4, 3, 0, 1, 2, // user-data [0] OCTET STRING
+                161, 17, 24, 15, // timestamp [1] GeneralizedTime OPTIONAL
+                    49, 57, 55, 48, 48, 49, 49, 50, 49, 51, 52, 54, 52, 48, 90,
+                164, 14, 48, 12, // s-address [4] HostAddress
+                    160, 3, 2, 1, 2,
+                    161, 5, 4, 3, 0, 1, 2,
+                165, 14, 48, 12, // r-address [5] HostAddress OPTIONAL
+                    160, 3, 2, 1, 3,
+                    161, 5, 4, 3, 1, 2, 3,
+            163, 15, 48, 13, // cksum [3] Checksum
+                160, 4, 2, 2, 1, 2,
+                161, 5, 4, 3, 0, 1, 2,
+    ];
+
+    assert_eq!(expected_encoding, msg.to_der().unwrap());
 }
