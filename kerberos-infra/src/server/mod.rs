bed03bd5d0bdec7c9c9aaebcdfb6727f9cfb326d
phatalways_sleeping <trankim147@gmail.com>
2024-08-19 00:52:56 +0700

    feat(version): add flags for kerberos-infra; fix ci.yml

--- kerberos-infra/src/server/mod.rs
+++ kerberos-infra/src/server/mod.rs
@@ -1,23 +1,23 @@
+mod builder;
 mod entry;
 mod errors;
 mod receiver;
 mod runnable;
-mod builder;
-mod utils;
-#[cfg(feature = "tcp")]
+#[cfg(feature = "server-tcp")]
 mod tcp;
 #[cfg(test)]
 mod tests;
-#[cfg(feature = "udp")]
+#[cfg(feature = "server-udp")]
 mod udp;
+mod utils;
 
+pub use builder::ServerBuilder;
 pub use errors::{KrbInfraSvrErr, KrbInfraSvrResult};
 pub use receiver::{AsyncReceiver, ExchangeError};
 pub use runnable::Runnable;
-pub use builder::ServerBuilder;
 
-#[cfg(feature = "tcp")]
+#[cfg(feature = "server-tcp")]
 pub use tcp::TcpServer;
 
-#[cfg(feature = "udp")]
+#[cfg(feature = "server-udp")]
 pub use udp::UdpServer;
