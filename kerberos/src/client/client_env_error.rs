48851d86760ce0b29a3f534638e611b809045c9e
Nguyen Nhu Hoai Nam <hoainamnguyennhu7@gmail.com>
2024-08-25 15:31:47 +0700

    initial client setup

---
+++ kerberos/src/client/client_env_error.rs
@@ -0,0 +1,15 @@
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug)]
+pub struct ClientEnvError {
+    message: String,
+}
+
+impl Display for ClientEnvError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "ClientEnvError: {}", self.message)
+    }
+}
+
+impl Error for ClientEnvError {}
