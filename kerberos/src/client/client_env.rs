829dfad924c4df49673d30c13116428ca8891fc5
Hoai Nam <hoainamnguyennhu7@gmail.com>
2024-08-29 23:34:07 +0700

    update clientenv trait

--- kerberos/src/client/client_env.rs
+++ kerberos/src/client/client_env.rs
@@ -1,17 +1,19 @@
-use messages::basic_types::{KerberosTime, OctetString};
+use messages::basic_types::{KerberosString, KerberosTime, OctetString};
 use std::fmt::{Debug, Display, Pointer};
 use crate::client::client_env_error::ClientEnvError;
 
 pub trait ClientEnv {
-    fn get_client_name(&self) -> Result<OctetString, ClientEnvError>;
+    fn get_client_name(&self) -> Result<KerberosString, ClientEnvError>;
 
-    fn get_client_realm(&self) -> Result<OctetString, ClientEnvError>;
+    fn get_client_realm(&self) -> Result<KerberosString, ClientEnvError>;
 
-    fn get_server_name(&self) -> Result<OctetString, ClientEnvError>;
+    fn get_server_name(&self) -> Result<KerberosString, ClientEnvError>;
 
-    fn get_server_realm(&self) -> Result<OctetString, ClientEnvError>;
+    fn get_server_realm(&self) -> Result<KerberosString, ClientEnvError>;
 
     fn get_client_address(&self) -> Result<OctetString, ClientEnvError>;
 
     fn get_current_time(&self) -> Result<KerberosTime, ClientEnvError>;
+
+    fn get_supported_etypes(&self) -> Result<Vec<i32>, ClientEnvError>;
 }
