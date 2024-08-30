b75ecda0db2a0b5e299619bf78da39c9a39620b0
Nguyen Nhu Hoai Nam <hoainamnguyennhu7@gmail.com>
2024-08-30 14:24:32 +0700

    update client env trait

--- kerberos/src/client/client_env.rs
+++ kerberos/src/client/client_env.rs
@@ -1,5 +1,7 @@
-use messages::basic_types::{KerberosString, KerberosTime, OctetString};
+use messages::basic_types::{EncryptionKey, KerberosString, KerberosTime, OctetString};
 use std::fmt::{Debug, Display, Pointer};
+use std::time::Duration;
+use messages::EncAsRepPart;
 use crate::client::client_env_error::ClientEnvError;
 
 pub trait ClientEnv {
@@ -16,4 +18,10 @@ pub trait ClientEnv {
     fn get_current_time(&self) -> Result<KerberosTime, ClientEnvError>;
 
     fn get_supported_etypes(&self) -> Result<Vec<i32>, ClientEnvError>;
+
+    fn get_client_key(&self, key_type: i32) -> Result<EncryptionKey, ClientEnvError>;
+
+    fn set_clock_diff(&self, diff: Duration, is_client_earlier: bool) -> Result<(), ClientEnvError>;
+
+    fn save_reply_data(&self, data: EncAsRepPart) -> Result<(), ClientEnvError>;
 }
