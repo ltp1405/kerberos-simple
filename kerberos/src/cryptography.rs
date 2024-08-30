678a669411733b99e7462f83d7c52cd5783845b9
Nguyen Nhu Hoai Nam <hoainamnguyennhu7@gmail.com>
2024-08-30 10:44:38 +0700

    add key to encrypt/decrypt

--- kerberos/src/cryptography.rs
+++ kerberos/src/cryptography.rs
@@ -1,7 +1,7 @@
 pub trait Cryptography {
-    fn encrypt(&self, data: &[u8]) -> Vec<u8>;
+    fn encrypt(&self, data: &[u8], key: &[u8]) -> Result<Vec<u8>, Err>;
 
-    fn decrypt(&self, data: &[u8]) -> Vec<u8>;
+    fn decrypt(&self, data: &[u8], key: &[u8]) -> Result<Vec<u8>, Err>;
 
-    fn generate_key(&self) -> Vec<u8>;
+    fn generate_key(&self) -> Result<Vec<u8>, Err>;
 }
