844ecb3470518be7158c720702f04561e7d1b87c
Nguyen Nhu Hoai Nam <hoainamnguyennhu7@gmail.com>
2024-09-05 14:53:08 +0700

    append authentication header in tgs request

--- kerberos/src/client/tgs_exchange.rs
+++ kerberos/src/client/tgs_exchange.rs
@@ -1,12 +1,15 @@
+use crate::client::ap_exchange::prepare_ap_request;
 use crate::client::client_env::ClientEnv;
 use crate::client::client_error::ClientError;
 use crate::client::util::generate_nonce;
-use messages::basic_types::{KerberosTime, NameTypes, PrincipalName};
-use messages::{KdcReqBodyBuilder, TgsReq};
+use crate::cryptography::Cryptography;
+use messages::basic_types::{KerberosTime, NameTypes, OctetString, PaData, PrincipalName};
+use messages::{Encode, KdcReqBodyBuilder, TgsReq};
 use std::time::Duration;
 
 pub fn prepare_tgs_request(
     client_env: impl ClientEnv,
+    cryptography: impl Cryptography,
 ) -> Result<TgsReq, ClientError> {
     let client_name = client_env.get_client_name()?;
     let server_realm = client_env.get_server_realm()?;
@@ -18,12 +21,22 @@ pub fn prepare_tgs_request(
     let nonce = generate_nonce();
     let current_time = client_env.get_current_time()?;
     let duration = Duration::new(60 * 60 * 24, 0);
-    let till = KerberosTime::from_unix_duration(
-        current_time + duration,
-    )
-    .or(Err(ClientError::DecodeError))?;
+    let till = KerberosTime::from_unix_duration(current_time + duration)
+        .or(Err(ClientError::DecodeError))?;
     let etypes = client_env.get_supported_etypes()?;
-    let pa_data = Vec::new();
+
+    // authentication header
+    let ap_req = prepare_ap_request(&client_env, &cryptography, false)?;
+    let mut ap_req_buf: Vec<u8> = Vec::new();
+    ap_req
+        .encode_to_vec(&mut ap_req_buf)
+        .or(Err(ClientError::EncodeError))?;
+    let auth_header = PaData::new(
+        1, // PaTgsReq
+        OctetString::new(ap_req_buf).or(Err(ClientError::EncodeError))?,
+    );
+    let pa_data = vec![auth_header];
+
     let req_body = KdcReqBodyBuilder::default()
         .cname(cname)
         .realm(server_realm)
