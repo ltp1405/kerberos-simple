d238e8d4d4b654e0b0ac2d245c2d553003dbff72
Hoai Nam <hoainamnguyennhu7@gmail.com>
2024-09-11 23:56:08 +0700

    impl missing client env functions

--- client_ui/src/client.rs
+++ client_ui/src/client.rs
@@ -3,9 +3,9 @@ use config::{Config, ConfigError};
 use kerberos::client::client_env::ClientEnv;
 use kerberos::client::client_env_error::ClientEnvError;
 use kerberos::cryptography::Cryptography;
-use messages::basic_types::{EncryptionKey, KerberosFlags};
+use messages::basic_types::{EncryptionKey, KerberosFlags, KerberosString, OctetString};
 use messages::flags::KdcOptionsFlag;
-use messages::{AsRep, Decode, Encode, TgsRep};
+use messages::{AsRep, Decode, EncAsRepPart, EncTgsRepPart, Encode, TgsRep};
 use serde::{Deserialize, Serialize};
 use std::fs;
 use std::io::Read;
@@ -43,7 +43,7 @@ impl Client {
 }
 
 impl ClientEnv for Client {
-    fn get_client_name(&self) -> Result<messages::basic_types::KerberosString, ClientEnvError> {
+    fn get_client_name(&self) -> Result<KerberosString, ClientEnvError> {
         self.cfg
             .name
             .clone()
@@ -53,7 +53,7 @@ impl ClientEnv for Client {
             })
     }
 
-    fn get_client_realm(&self) -> Result<messages::basic_types::KerberosString, ClientEnvError> {
+    fn get_client_realm(&self) -> Result<KerberosString, ClientEnvError> {
         self.cfg
             .realm
             .clone()
@@ -63,15 +63,23 @@ impl ClientEnv for Client {
             })
     }
 
-    fn get_server_name(&self) -> Result<messages::basic_types::KerberosString, ClientEnvError> {
+    fn get_server_name(&self) -> Result<KerberosString, ClientEnvError> {
+        KerberosString::new(self.server_name.as_bytes()).or(Err(ClientEnvError {
+            message: "Failed to get server name".to_string(),
+        }))
+    }
+
+    fn get_server_realm(&self) -> Result<KerberosString, ClientEnvError> {
+        KerberosString::new(self.server_realm.as_bytes()).or(Err(ClientEnvError {
+            message: "Failed to get server realm".to_string(),
+        }))
+    }
+
+    fn get_client_address(&self) -> Result<OctetString, ClientEnvError> {
         todo!()
     }
 
-    fn get_server_realm(&self) -> Result<messages::basic_types::KerberosString, ClientEnvError> {
-        todo!()
-    }
-
-    fn get_kdc_options(&self) -> Result<messages::basic_types::KerberosFlags, ClientEnvError> {
+    fn get_kdc_options(&self) -> Result<KerberosFlags, ClientEnvError> {
         let mut flag = KerberosFlags::builder();
         if self.renewable {
             flag.set(KdcOptionsFlag::RENEWABLE as usize);
@@ -87,17 +95,14 @@ impl ClientEnv for Client {
         todo!()
     }
 
-    fn get_client_key(
-        &self,
-        key_type: i32,
-    ) -> Result<messages::basic_types::EncryptionKey, ClientEnvError> {
+    fn get_client_key(&self, key_type: i32) -> Result<EncryptionKey, ClientEnvError> {
         let mut loc = self
             .cfg
             .cache_location
             .clone()
             .unwrap_or(PathBuf::from("./"));
         loc.push("key");
-        let mut f = std::fs::File::open(loc).map_err(|_| ClientEnvError {
+        let mut f = fs::File::open(loc).map_err(|_| ClientEnvError {
             message: "Failed to fetch key".to_string(),
         })?;
         let mut buf = vec![];
@@ -113,47 +118,106 @@ impl ClientEnv for Client {
         todo!()
     }
 
-    fn save_as_reply(&self, data: &messages::AsRep) -> Result<(), ClientEnvError> {
+    fn save_as_reply(&self, data: &AsRep) -> Result<(), ClientEnvError> {
+        let enc_part = data.enc_part().clone();
+        let decrypted_enc_part = EncAsRepPart::from_der(
+            &self
+                .get_crypto(*enc_part.etype())?
+                .decrypt(
+                    enc_part.cipher().as_ref(),
+                    self.get_client_key(1)?.keyvalue().as_ref(),
+                )
+                .or(Err(ClientEnvError {
+                    message: "Failed to decrypt".to_string(),
+                }))?,
+        )
+        .or(Err(ClientEnvError {
+            message: "Decode error".to_string(),
+        }))?;
+        self.open_file_and_write("as_rep_enc_part", &decrypted_enc_part.to_der().unwrap())
+            .map_err(|_| ClientEnvError {
+                message: "".to_string(),
+            })?;
         self.open_file_and_write("as_rep", &data.to_der().unwrap())
             .map_err(|_| ClientEnvError {
                 message: "".to_string(),
             })
     }
 
+    fn save_as_reply_enc_part(&self, data: &EncAsRepPart) -> Result<(), ClientEnvError> {
+        todo!()
+    }
+
     fn get_as_reply(&self) -> Result<AsRep, ClientEnvError> {
         self.open_file_and_read("as_rep")
             .map(|b| AsRep::from_der(&b).unwrap())
             .map_err(|_| ClientEnvError {
-                message: "".to_string(),
+                message: "Failed to read file".to_string(),
             })
     }
 
-    fn save_tgs_reply(&self, data: &messages::TgsRep) -> Result<(), ClientEnvError> {
+    fn get_as_reply_enc_part(&self) -> Result<EncAsRepPart, ClientEnvError> {
+        self.open_file_and_read("as_rep_enc_part")
+            .map(|b| EncAsRepPart::from_der(&b).unwrap())
+            .map_err(|_| ClientEnvError {
+                message: "Failed to read file".to_string(),
+            })
+    }
+
+    fn save_tgs_reply(&self, data: &TgsRep) -> Result<(), ClientEnvError> {
+        let enc_part = data.enc_part().clone();
+        let decrypted_enc_part = EncTgsRepPart::from_der(
+            &self
+                .get_crypto(*enc_part.etype())?
+                .decrypt(enc_part.cipher().as_ref(), [0u8; 4].as_slice())
+                .or(Err(ClientEnvError {
+                    message: "Failed to decrypt".to_string(),
+                }))?,
+        )
+        .or(Err(ClientEnvError {
+            message: "Decode error".to_string(),
+        }))?;
+        self.open_file_and_write("tgs_rep_enc_part", &decrypted_enc_part.to_der().unwrap())
+            .map_err(|_| ClientEnvError {
+                message: "Failed to read file".to_string(),
+            })?;
         self.open_file_and_write("tgs_rep", &data.to_der().unwrap())
             .map_err(|_| ClientEnvError {
-                message: "".to_string(),
+                message: "Failed to read file".to_string(),
             })
     }
 
-    fn get_tgs_reply(&self) -> Result<messages::TgsRep, ClientEnvError> {
+    fn save_tgs_reply_enc_part(&self, data: &EncTgsRepPart) -> Result<(), ClientEnvError> {
+        todo!()
+    }
+
+    fn get_tgs_reply(&self) -> Result<TgsRep, ClientEnvError> {
         self.open_file_and_read("tgs_rep")
             .map(|b| TgsRep::from_der(&b).unwrap())
             .map_err(|_| ClientEnvError {
-                message: "".to_string(),
+                message: "Failed to read file".to_string(),
+            })
+    }
+
+    fn get_tgs_reply_enc_part(&self) -> Result<EncTgsRepPart, ClientEnvError> {
+        self.open_file_and_read("tgs_rep_enc_part")
+            .map(|b| EncTgsRepPart::from_der(&b).unwrap())
+            .map_err(|_| ClientEnvError {
+                message: "Failed to read file".to_string(),
             })
     }
 
     fn save_subkey(&self, key: EncryptionKey) -> Result<(), ClientEnvError> {
         self.open_file_and_write("subkey", &key.to_der().unwrap())
             .map_err(|_| ClientEnvError {
-                message: "".to_string(),
+                message: "Failed to read file".to_string(),
             })
     }
 
     fn save_seq_number(&self, seq_num: u32) -> Result<(), ClientEnvError> {
         self.open_file_and_write("seq_number", &seq_num.to_der().unwrap())
             .map_err(|_| ClientEnvError {
-                message: "".to_string(),
+                message: "Failed to read file".to_string(),
             })
     }
 }
