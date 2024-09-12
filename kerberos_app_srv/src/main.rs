207c0b390261405adfc62cb1319e393da4473311
nguyentrinhduy <duy.nguyentrinh@sun.studio>
2024-09-12 23:50:20 +0700

    initialize kerberos srv app

--- kerberos_app_srv/src/main.rs
+++ kerberos_app_srv/src/main.rs
@@ -1,17 +1,28 @@
-use actix_web::{web, App, HttpServer, Responder};
+use std::sync::Arc;
 
-async fn index() -> impl Responder {
+use actix_web::{web, App, HttpServer, Responder};
+use database::AppDbSchema;
+use kerberos_infra::server::{DbSettings, Migration, PgDbSettings, PostgresDb};
+
+async fn index(data: web::Data<Arc<PostgresDb>>) -> impl Responder {
     "Hello world!"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(
+    let config = PgDbSettings::load_from_dir();
+    let schema = AppDbSchema::boxed();
+
+    let mut postgre = PostgresDb::new(config, schema);
+    postgre.migrate_then_seed().await.unwrap();
+    let db = Arc::new(postgre);
+    HttpServer::new(move || {
+        App::new().app_data(web::Data::new(db.clone())).service(
             // prefixes all resources and routes attached to it...
             web::scope("/app")
                 // ...so this handles requests for `GET /app/index.html`
-                .route("/index.html", web::get().to(index)),
+                .route("/index.html", web::get().to(index))
+                .route("/index.html", web::post().to(index)),
         )
     })
     .bind(("127.0.0.1", 8080))?
