81800392acebdb9e068770d922a902b665cf9e30
phatalways_sleeping <trankim147@gmail.com>
2024-09-13 18:58:13 +0700

    fix: migrate current unit tests to async version

--- kerberos/src/authentication_service/tests.rs
+++ kerberos/src/authentication_service/tests.rs
@@ -1,14 +1,15 @@
 use crate::authentication_service::AuthenticationServiceBuilder;
 use crate::cryptography::Cryptography;
 use crate::cryptography_error::CryptographyError;
+use crate::service_traits::{PrincipalDatabase, PrincipalDatabaseRecord};
+use async_trait::async_trait;
 use messages::basic_types::{
-    AddressTypes, EncryptionKey, HostAddress, Int32, KerberosFlags, KerberosFlagsBuilder,
-    KerberosString, KerberosTime, NameTypes, OctetString, PrincipalName, Realm,
+    EncryptionKey, KerberosFlags, KerberosString, KerberosTime, NameTypes, OctetString,
+    PrincipalName, Realm,
 };
 use messages::flags::KdcOptionsFlag;
-use messages::{AsReq, KdcReqBody, KdcReqBodyBuilder};
+use messages::{AsReq, KdcReqBodyBuilder};
 use std::time::Duration;
-use crate::service_traits::{PrincipalDatabase, PrincipalDatabaseRecord};
 
 struct MockedCrypto;
 
@@ -17,7 +18,7 @@ impl Cryptography for MockedCrypto {
         1
     }
     fn encrypt(&self, data: &[u8], key: &[u8]) -> Result<Vec<u8>, CryptographyError> {
-        let mut data = data.to_vec();
+        let data = data.to_vec();
         let data = data
             .iter()
             .zip(key.iter().cycle())
@@ -33,30 +34,33 @@ impl Cryptography for MockedCrypto {
     fn generate_key(&self) -> Result<Vec<u8>, CryptographyError> {
         Ok(vec![0xff; 8])
     }
+
+    fn clone_box(&self) -> Box<dyn Cryptography> {
+        Box::new(MockedCrypto)
+    }
 }
 
 struct MockedPrincipalDb;
 
+#[async_trait]
 impl PrincipalDatabase for MockedPrincipalDb {
-    fn get_principal(
+    async fn get_principal(
         &self,
-        principal_name: &PrincipalName,
-        realm: &Realm,
+        _principal_name: &PrincipalName,
+        _realm: &Realm,
     ) -> Option<PrincipalDatabaseRecord> {
-        Some(
-            PrincipalDatabaseRecord {
-                max_lifetime: Duration::from_secs(5 * 60),
-                key: EncryptionKey::new(1, OctetString::new(vec![1; 8]).unwrap()),
-                p_kvno: Some(1),
-                max_renewable_life: Duration::from_secs(5 * 60),
-                supported_encryption_types: vec![1, 2, 3],
-            },
-        )
+        Some(PrincipalDatabaseRecord {
+            max_lifetime: Duration::from_secs(5 * 60),
+            key: EncryptionKey::new(1, OctetString::new(vec![1; 8]).unwrap()),
+            p_kvno: Some(1),
+            max_renewable_life: Duration::from_secs(5 * 60),
+            supported_encryption_types: vec![1, 2, 3],
+        })
     }
 }
 
-#[test]
-fn dummy_test() {
+#[tokio::test]
+async fn dummy_test() {
     let kdc_req_body = KdcReqBodyBuilder::default()
         .kdc_options(
             KerberosFlags::builder()
@@ -101,14 +105,5 @@ fn dummy_test() {
         )
         .build()
         .unwrap();
-    auth_service
-        .handle_krb_as_req(
-            HostAddress::new(
-                AddressTypes::Ipv4,
-                OctetString::new("192.168.64.184".as_bytes()).unwrap(),
-            )
-            .unwrap(),
-            &as_req,
-        )
-        .unwrap();
+    auth_service.handle_krb_as_req(&as_req).await.unwrap();
 }
