ae6e22109d03446187913ca993c3d5eeb0b2167a
nguyentrinhduy <duy.nguyentrinh@sun.studio>
2024-09-13 21:50:53 +0700

    switch to arc mutex for replay cache in http server

--- kerberos_app_srv/src/replay_cache/replay_cache.rs
+++ kerberos_app_srv/src/replay_cache/replay_cache.rs
@@ -1,19 +1,20 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
 use async_trait::async_trait;
 use kerberos::service_traits::{ApReplayCache, ApReplayEntry};
 use messages::Encode;
+use tokio::sync::Mutex;
 
 use super::error::AppServerReplayCacheError;
 
 pub struct AppServerReplayCache {
-    pub cache: HashMap<Vec<u8>, bool>,
+    pub cache: Arc<Mutex<HashMap<Vec<u8>, bool>>>,
 }
 
 impl AppServerReplayCache {
     pub fn new() -> AppServerReplayCache {
         AppServerReplayCache {
-            cache: HashMap::new(),
+            cache: Arc::new(Mutex::new(HashMap::new())),
         }
     }
 }
@@ -22,13 +23,13 @@ impl AppServerReplayCache {
 impl ApReplayCache for AppServerReplayCache {
     type ApReplayCacheError = AppServerReplayCacheError;
 
-    async fn store(&mut self, authenticator: &ApReplayEntry) -> Result<(), Self::ApReplayCacheError> {
-        self.cache.insert(authenticator.to_der().unwrap(), true);
+    async fn store(&self, authenticator: &ApReplayEntry) -> Result<(), Self::ApReplayCacheError> {
+        self.cache.try_lock().unwrap().insert(authenticator.to_der().unwrap(), true);
         Ok(())
     }
 
     async fn contain(&self, authenticator: &ApReplayEntry) -> Result<bool, Self::ApReplayCacheError> {
-        let is_contained_key = self.cache.contains_key(&authenticator.to_der().unwrap());
+        let is_contained_key = self.cache.try_lock().unwrap().contains_key(&authenticator.to_der().unwrap());
         Ok(is_contained_key)
     }
 }
